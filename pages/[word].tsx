37cdc23897ab8bc7cde0a8ff034487bb10faed8c
Lawrence Ughonu <lawrenceughonu@gmail.com>
2022-10-17 09:33:11 +0100

    increase size of text

--- pages/[word].tsx
+++ pages/[word].tsx
@@ -43,7 +43,7 @@ router.push(`/${data[randomNum]}`)
       <div className='bg-gradient-to-br from-blue-300 to-blue-500 pb-5 h-screen text-white px-24   ' >
       <p>Random Number: { count }</p>
       <button onClick={toggle} className='bg-[#f7d78c] text-gray-900 px-8 py-2 rounded mx-auto block'>Show text</button>
-      <div className=''>
+      <div className='text-center text-6xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
         {/* {index !== undefined &&  data[index || 0 ].toUpperCase()} */ }
         {word.toUpperCase()}
       </div>
